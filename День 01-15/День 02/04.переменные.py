3b1cbd3633cb3c62547e1cb631d74c88d3b528a7
BEPb <andrej.marinchenko@gmail.com>
2021-11-03 22:54:23 +0300

    01. День

---
+++ День 01-15/День 02/04.переменные.py
@@ -0,0 +1,26 @@
+"""
+Общие операции с переменными (определение типа переменной)
+Название файла '04.переменные.py'
+
+Version: 0.1
+Author: Andrej Marinchenko
+Date: 2021-11-03
+"""
+
+# определяем значения переменных
+a = 9999
+b = -56
+c = 69.74
+d = 2 + 3j
+e = 'Ю'
+f = 'Привет, мир'
+g = True
+
+# определяем типы переменных
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
---
+++ День 01-15/День 02/05.переменные.py
@@ -0,0 +1,45 @@
+"""
+Общие операции с переменными
+Название файла '05.переменные.py'
+
+Version: 0.1
+Author: Andrej Marinchenko
+Date: 2021-11-03
+"""
+
+a = 100
+b = str(a)
+c = 12.345
+d = str(c)
+e = '123'
+f = int(e)
+g = '123.456'
+h = float(g)
+i = False
+j = str(i)
+k = 'hello'
+m = bool(k)
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+print(c)
+print(type(c))
+print(d)
+print(type(d))
+print(e)
+print(type(e))
+print(f)
+print(type(f))
+print(g)
+print(type(g))
+print(h)
+print(type(h))
+print(i)
+print(type(i))
+print(j)
+print(type(j))
+print(k)
+print(type(k))
+print(m)
+print(type(m))

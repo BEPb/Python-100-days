e7c981bcdf60f0c448bf92f808b119bd5ca6c865
BEPb <andrej.marinchenko@gmail.com>
2021-11-08 01:08:13 +0300

    07. день

---
+++ День 01-15/День 07/05.треугольник_Ян_Хуэя.py
@@ -0,0 +1,34 @@
+"""
+Выведите 10 строк треугольника Ян Хуэя - n-го коэффициента расширения двучлена.
+1
+1 1
+1 2 1
+1 3 3 1
+1 4 6 4 1
+... ... ...
+
+
+Название файла '05.треугольник_Ян_Хуэя.py'
+
+Version: 0.1
+Author: Andrej Marinchenko
+Date: 2021-11-07
+"""
+
+
+def main():  # главная функция
+    num = int(input('Введите число строк: '))
+    yh = [[]] * num
+    for row in range(len(yh)):
+        yh[row] = [None] * (row + 1)
+        for col in range(len(yh[row])):
+            if col == 0 or col == row:
+                yh[row][col] = 1
+            else:
+                yh[row][col] = yh[row - 1][col] + yh[row - 1][col - 1]
+            print(yh[row][col], end='\t')
+        print()
+
+
+if __name__ == '__main__':  # если запускаемая программа - эта (имя запущенной программы соответствует этой программе)
+    main()  # то выполняем главную функцию

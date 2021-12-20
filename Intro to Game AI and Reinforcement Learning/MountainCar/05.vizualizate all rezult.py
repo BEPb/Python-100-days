1db174a6357f51fa575e984a3e0c84dbe8f3de93
BEPb <andrej.marinchenko@gmail.com>
2021-12-20 20:33:44 +0300

    Reinforcement Learning -  vizualizate

---
+++ Intro to Game AI and Reinforcement Learning/MountainCar/05.vizualizate all rezult.py
@@ -0,0 +1,50 @@
+"""
+Python 3.9 программа на Python по изучению обучения с подкреплением - Reinforcement Learning
+Название файла 05.vizualizate all rezult.py
+преобразует все таблицы в графики активности, где зеленый - максимальная награда, красный - все
+остальное
+
+
+Version: 0.1
+Author: Andrej Marinchenko
+Date: 2021-12-20
+"""
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+from matplotlib import style
+import numpy as np
+
+style.use('ggplot')
+
+
+def get_q_color(value, vals):
+    if value == max(vals):
+        return "green", 1.0
+    else:
+        return "red", 0.3
+
+
+fig = plt.figure(figsize=(12, 9))
+
+
+for i in range(0, 25000, 10):
+    print(i)
+    ax1 = fig.add_subplot(311)
+    ax2 = fig.add_subplot(312)
+    ax3 = fig.add_subplot(313)
+
+    q_table = np.load(f"qtables/{i}-qtable.npy")
+
+    for x, x_vals in enumerate(q_table):
+        for y, y_vals in enumerate(x_vals):
+            ax1.scatter(x, y, c=get_q_color(y_vals[0], y_vals)[0], marker="o", alpha=get_q_color(y_vals[0], y_vals)[1])
+            ax2.scatter(x, y, c=get_q_color(y_vals[1], y_vals)[0], marker="o", alpha=get_q_color(y_vals[1], y_vals)[1])
+            ax3.scatter(x, y, c=get_q_color(y_vals[2], y_vals)[0], marker="o", alpha=get_q_color(y_vals[2], y_vals)[1])
+
+            ax1.set_ylabel("Action 0")
+            ax2.set_ylabel("Action 1")
+            ax3.set_ylabel("Action 2")
+
+    #plt.show()
+    plt.savefig(f"qtable_charts/{i}.png")
+    plt.clf()

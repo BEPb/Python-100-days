63dd9cf6e5afb1346d36a278357a21b965e84d2b
BEPb <andrej.marinchenko@gmail.com>
2021-12-20 20:36:32 +0300

    Reinforcement Learning -  vizualizate

---
+++ Intro to Game AI and Reinforcement Learning/MountainCar/06. make_video.py
@@ -0,0 +1,21 @@
+import cv2
+import os
+
+
+def make_video():
+    # windows:
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    # Linux:
+    #fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
+    out = cv2.VideoWriter('qlearn.avi', fourcc, 60.0, (1200, 900))
+
+    for i in range(0, 14000, 10):
+        img_path = f"qtable_charts/{i}.png"
+        print(img_path)
+        frame = cv2.imread(img_path)
+        out.write(frame)
+
+    out.release()
+
+
+make_video()

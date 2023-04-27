5cd1fd5e225031ed0e248f51bd3b58f0b2aa5b1c
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2023-04-27 08:53:29 +0300

    upd day 15

--- День 01-15/День 15/02.Excel.py
+++ День 01-15/День 15/02.Excel.py
@@ -1,21 +1,21 @@
 """
-Python 3.9 чтение Excel документа
+Python 3.10 чтение Excel документа
 Название файла '02.Excel.py'
 
 Version: 0.1
 Author: Andrej Marinchenko
-Date: 2021-11-13
+Date: 2023-04-27
 """
 
-from openpyxl import load_workbook
-from openpyxl import Workbook
+from openpyxl import load_workbook  # импорт библиотеки для чтения
 
-workbook = load_workbook('./res/99.xlsx')
-print(workbook.sheetnames)
-sheet = workbook[workbook.sheetnames[0]]
-print(sheet.title)
-for row in range(2, 7):
-    for col in range(65, 70):
-        cell_index = chr(col) + str(row)
-        print(sheet[cell_index].value, end='\t')
-    print()
+workbook = load_workbook('./res/99.xlsx')  # считываем файл
+print(workbook.sheetnames)  # печать названий всех листов файла (их два ['название листа файла эксель', 'лист номер 2'])
+sheet = workbook[workbook.sheetnames[0]]  # присвоим переменной название первого листа
+print(sheet.title)  # печать названия первого листа
+
+for row in range(2, 7):  # используем цикл для прохода по строкам со 2-й по 6-ю
+    for col in range(65, 70):   # используем цикл для прохода по колонкам с 65-й по 70-ю
+        cell_index = chr(col) + str(row)  # получаем индекс ячейки номер колонки + номер строки
+        print(sheet[cell_index].value, end='\t')  # печатаем все содержимые ячейки в одной строке
+    print()  # переход на новую строку
